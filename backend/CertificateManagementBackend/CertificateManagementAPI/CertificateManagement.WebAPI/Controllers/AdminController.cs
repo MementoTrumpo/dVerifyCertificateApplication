066e9b960ade1268a9d256407c41cc098829e184
MementoTrumpo <98746183+MementoTrumpo@users.noreply.github.com>
2025-04-15 23:06:05 +0300

    Добавлены модули tailwind для красивого оформления web интерфейса,

---
+++ backend/CertificateManagementBackend/CertificateManagementAPI/CertificateManagement.WebAPI/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+using CertificateManagement.WebAPI.Contexts;
+using CertificateManagement.WebAPI.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CertificateManagement.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/admin")]
+[Authorize(Roles = "Admin")]
+public class AdminController : ControllerBase
+{
+    private readonly CertificateDbContext _context;
+
+    public AdminController(CertificateDbContext context)
+    {
+        _context = context;
+    }
+        
+    [HttpPost("setRole")]
+    public async Task<IActionResult> SetUserRole([FromBody] SetUpUserRoleRequest request, CancellationToken cancellationToken)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => 
+            u.WalletAddress.ToLower() == request.WalletAddress.ToLower(), cancellationToken);
+        if (user is null)
+        {
+            return NotFound("Пользователь не найден!");
+        }
+        
+        user.Role = request.NewRole;
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Ok("Роль назначена!");
+    }
+
+    [HttpGet]
+    [Route("users")]
+    public async Task<IActionResult> GetUsers(CancellationToken cancellationToken)
+    {
+        var users = await _context.Users
+            .Select(u => new
+            {
+                u.WalletAddress,
+                Role = u.Role.ToString()
+            })
+            .ToListAsync(cancellationToken);
+        return Ok(users);
+    }
+}
